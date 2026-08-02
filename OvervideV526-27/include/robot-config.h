a6b247a7bf45a381fa174dfd74dd97ae0bf89d63
RyanV5Robo <ryan.jun.chu@gmail.com>
2026-08-02 11:49:54 -0700

    Configuration changes

--- OvervideV526-27/include/robot-config.h
+++ OvervideV526-27/include/robot-config.h
@@ -7,6 +7,22 @@ extern brain Brain;
 
 //Add your devices below, and don't forget to do the same in robot-config.cpp:
 
+extern motor LeftFront;
+extern motor LeftBack;
+extern motor RightFront;
+extern motor RightBack;
+extern motor Lift1;
+extern motor Lift2;
+extern motor Intake;
+extern motor Claw;
+extern motor Clawlift;
 
+extern motor_group LeftDrive;
+extern motor_group RightDrive;
+extern motor_group Lift;
+
+extern inertial Gyro;
+
+controller Controller1 = controller(primary);
 
 void  vexcodeInit( void );
--- OvervideV526-27/src/robot-config.cpp
+++ OvervideV526-27/src/robot-config.cpp
@@ -12,23 +12,21 @@ brain Brain;
 
 //Add your devices below, and don't forget to do the same in robot-config.h:
 
-motor LeftFront = motor(PORT13, ratio4_3, false);
-motor LeftBack = motor(PORT14, ratio4_3, false);
-motor RightFront = motor(PORT11, ratio4_3, true);
-motor RightBack = motor(PORT12, ratio4_3, true);
-motor Lift1 = motor(PORT18, ratio2_1, true);
-motor Lift2 = motor(PORT19, ratio2_1, false);
+motor LeftFront = motor(PORT13, ratio6_1, false);
+motor LeftBack = motor(PORT14, ratio6_1, false);
+motor RightFront = motor(PORT11, ratio6_1, true);
+motor RightBack = motor(PORT12, ratio6_1, true);
+motor Lift1 = motor(PORT18, ratio18_1, true);
+motor Lift2 = motor(PORT19, ratio18_1, false);
 motor Intake = motor(PORT20, ratio6_1, true);
-motor Claw = motor(PORT4, ratio2_1, true);
-motor Clawlift = motor(PORT3, ratio2_1, true);
+motor Claw = motor(PORT4, ratio18_1, true);
+motor Clawlift = motor(PORT3, ratio18_1, true);
 
 motor_group LeftDrive = motor_group(LeftFront, LeftBack);
 motor_group RightDrive = motor_group(RightFront, RightBack);
 motor_group Lift = motor_group(Lift1, Lift2);
 
 inertial Gyro = inertial(PORT21);
-
-Drive chassis = chassis(LeftDrive, RightDrive, 2.75, 320, 40, mm, 1);
 
 controller Controller1 = controller(primary);
 
